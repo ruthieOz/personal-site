09e4ab110020f68c6277cffa04c15c2eeed9f288
Ruth Idam <105409585+ruthieOz@users.noreply.github.com>
2022-07-04 15:46:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 favouriteMovieGenre("fantasy")
 
 // watermelon, tomato, banana, orange, avocado, blueberry
-favouriteFruit("tomato avocado")
+favouriteFruit("tomato")
 
 // light, dark
 favouriteMode("dark")
